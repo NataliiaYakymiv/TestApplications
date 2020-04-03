bdb825bf68078f428b13343cc4972d1ddd6516e2
yakymiv.nataliia <yakymivnatalya@gmail.com>
2020-04-03 13:23:50 +0300

    Initial commit

---
+++ TestTask_Conscensia/MyTask_WebService/Controllers/GeneratedNumberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MyTask_WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeneratedNumberController : ControllerBase
+    {
+        // GET: api/GeneratedNumber
+        [HttpGet]
+        public int Get()
+        {
+            return new Random().Next(1, 100);
+        }
+    }
+}
---
+++ TestTask_Conscensia/WpfGetYourNumber/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows;
+
+namespace WpfGetYourNumber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static System.Timers.Timer aTimer;
+
+        HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            aTimer = new System.Timers.Timer();
+            aTimer.Interval = 1000;
+
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+
+            // Have the timer fire repeated events (true is the default)
+            aTimer.AutoReset = true;
+        }
+
+        public delegate void UpdateValueCallback(int value);
+
+        private void UpdateValue(int value)
+        {
+            sliderName.Value = value;
+        }
+
+        private async void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            HttpResponseMessage response = client.GetAsync("https://localhost:44307/generatednumber").Result;
+
+            string rundomNumber = await response.Content.ReadAsStringAsync();
+            System.Diagnostics.Trace.WriteLine(rundomNumber);
+
+            sliderName.Dispatcher.Invoke(
+            new UpdateValueCallback(this.UpdateValue),
+            Int32.Parse(rundomNumber)
+            );
+        }
+        
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            aTimer.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            aTimer.Enabled = false;
+        }
+
+    }
+}
